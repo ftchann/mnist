22e7ed2d7009aa0c3da052b638bd9e0d434246d5
ftchann <yann_huynh@hotmail.ch>
2018-09-23 13:58:21 +0200

    mnist

--- untitled0.py
+++ untitled0.py
@@ -1,14 +1,2 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 20 22:15:36 2018
 
-@author: Yann
-"""
-
-import numpy as np
-import matplotlib.pyplot as plot
-np.random.seed(1)
-a=(np.random.normal(0.0,pow(20, -0.5),(20,1)))
-c=np.random.rand(20,1)
-d=np.sort(c)
-print(d)
+import numpy
