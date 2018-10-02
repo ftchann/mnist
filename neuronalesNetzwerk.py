52d458900eca2e15dcc77ee35d58227c2c4765e3
ftchann <yann_huynh@hotmail.ch>
2018-10-02 17:32:07 +0200

    comment

--- neuronalesNetzwerk.py
+++ neuronalesNetzwerk.py
@@ -44,6 +44,7 @@ class neuralNetwork:
     #Leaky ReLu
     def lrelu(self, x):
         return x * (x > 0) + (x <= 0) * 0.01*x
+    #Ableitung Leaky Relu
     def lrelu_derivative(self, x):
         return 1 * (x > 0) + (x <= 0) * 0.01
     #neuronales Netzwerk inistialisieren
